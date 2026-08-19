0837b509476f8a0fd6ad4ced6ebb39848333a1ff
jambrocio-202544 <jambrocio-2025444@Kinal.edu.gt>
2026-08-19 00:23:59 -0600

    fix : "corrige detección de expiración de sesión y logout manual"

--- Control-Gastos/frontend/src/app/app.component.ts
+++ Control-Gastos/frontend/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<router-outlet></router-outlet>`,
+    template: `<router-outlet></router-outlet>
+            <app-session-expired></app-session-expired>`,
     standalone: false
 })
 export class AppComponent {}
