c71949599a648144d1f8c4ee5bbfb611e2b9050c
jambrocio-202544 <jambrocio-2025444@Kinal.edu.gt>
2026-08-31 22:43:44 -0600

    feat: se implementa una nuevo endpoint para el apartado de ingresos

--- Control-Gastos/backend/src/app.ts
+++ Control-Gastos/backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
+import incomeRoutes from './routes/income.routes';
 
 dotenv.config();
 
@@ -17,5 +18,7 @@ app.use('/api/auth', authRoutes);
 app.get('/api/health', (_req, res) => {
   res.json({ status: 'OK' });
 });
+
+app.use('/api/incomes', incomeRoutes)
 
 export default app;
